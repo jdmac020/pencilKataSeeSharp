3fa0062bad4255b29298789a18ae75542d692bf2
John MacGregor <jmacgregor@revlocal.com>
2021-11-10 14:37:49 -0500

    Write content to paper with pencil

---
+++ Desk.Tests/PencilShould.cs
@@ -0,0 +1,24 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desk.Tests
+{
+    public class PencilShould
+    {
+        [Fact]
+        public void WriteStrings()
+        {
+            var paper = new Paper();
+            var pencil = new Pencil();
+
+            pencil.Write("Test String", paper);
+
+            paper.Content.ShouldBe("Test String");
+        }
+    }
+}
---
+++ Desk/Pencil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desk
+{
+    public class Pencil
+    {
+        public void Write(string note, Paper paper)
+        {
+            paper.Content = note;
+        }
+    }
+}
